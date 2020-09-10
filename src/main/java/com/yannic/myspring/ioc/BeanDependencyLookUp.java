fe02b4f2fd6fb5f20da9e44b235f1c1038c0d4a0
yannic <1243701904@qq.com>
2020-09-10 08:43:23 +0800

    bean依赖查找

--- src/main/java/com/yannic/myspring/ioc/BeanDependencyLookUp.java
+++ src/main/java/com/yannic/myspring/ioc/BeanDependencyLookUp.java
@@ -2,6 +2,7 @@ package com.yannic.myspring.ioc;
 
 import com.yannic.myspring.ioc.model.User;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -9,12 +10,39 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class BeanDependencyLookUp {
     public static void main(String[] args) {
-        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:beanLookUp.xml");
-        lookUpByName(beanFactory);
+        // 启动Spring 应用上下文
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:META-INF/beanLookUp.xml");
+        lookUpByType(beanFactory);
+        lookUpLazy(beanFactory);
+        lookUpCollectionType(beanFactory);
     }
-    public static void lookUpByName(BeanFactory beanFactory){
-        User user = beanFactory.getBean(User.class);
-        System.out.println(user.toString());
 
+    /**
+     * 延迟查找
+     *
+     * @param beanFactory
+     */
+    private static void lookUpLazy(BeanFactory beanFactory) {
+        ObjectFactory objectFactory = (ObjectFactory) beanFactory.getBean("objectFactory");
+        User user = (User) objectFactory.getObject();
+        System.out.println("延迟查找" + user.toString());
+    }
+
+    /**
+     * 按类型查找
+     *
+     * @param beanFactory
+     */
+    public static void lookUpByType(BeanFactory beanFactory) {
+        User user = beanFactory.getBean(User.class);
+        System.out.println("实时查找" + user.toString());
+    }
+
+
+    /**
+     * bean集合
+     * @param beanFactory
+     */
+    private static void lookUpCollectionType(BeanFactory beanFactory) {
     }
 }
---
+++ src/main/java/com/yannic/myspring/ioc/model/SuperUser.java
@@ -0,0 +1,20 @@
+package com.yannic.myspring.ioc.model;
+
+public class SuperUser extends User{
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "SuperUser{" +
+                "address='" + address + '\'' +
+                "} " + super.toString();
+    }
+}
